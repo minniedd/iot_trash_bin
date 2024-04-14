810a70e8bed73fc751c01e4b548756b89fcbd00a
Amina Bardak <48387428+minniedd@users.noreply.github.com>
2024-04-14 17:21:55 +0200

    Update main1.ino

--- main1.ino
+++ main1.ino
@@ -1,15 +1,37 @@
 #include <ESP8266WiFi.h>
 #include <LiquidCrystal_I2C.h>
-#include <FirebaseArduino.h>
+#include <FirebaseESP8266.h>
 LiquidCrystal_I2C lcd(0x27, 16, 2);
 
+//Provide the token generation process info.
+#include <addons/TokenHelper.h>
 
-#define FIREBASE_HOST ""
-#define FIREBASE_AUTH ""
+//Provide the RTDB payload printing info and other helper functions.
+#include <addons/RTDBHelper.h>
 
-// wifi
-char ssid[] = "SSID"; //SSID
-char pass[] = "PASS"; // Password
+// Define WIFI
+#define WIFI_SSID "" //SSID
+#define WIFI_PASSWORD "" // Password
+
+/* API Key */
+#define API_KEY ""
+
+/* RTDB URL */
+#define DATABASE_URL "iot-mina-default-rtdb.firebaseio.com" 
+
+/* User INFO */
+#define USER_EMAIL ""
+#define USER_PASSWORD ""
+
+//define Firebase Data object
+FirebaseData fbdo;
+
+FirebaseAuth auth;
+FirebaseConfig config;
+
+unsigned long sendDataPrevMillis = 0;
+
+unsigned long count = 0;
 
 // garbage bin heights
 const int total_height = 21;
@@ -30,8 +52,6 @@ int snsr_to_max = 0;
 const int Field_number = 1; 
 WiFiClient  client;
 
-FirebaseArduino firebase;
-
 
 void setup(){
   lcd.init();
@@ -48,12 +68,45 @@ void setup(){
 
   snsr_to_max = total_height - hold_height;
 
-  Firebase.begin(FIREBASE_HOST, FIREBASE_AUTH);
-  delay(2500);
+  Serial.begin(115200);
+
+  WiFi.begin(WIFI_SSID, WIFI_PASSWORD);
+  Serial.print("Connecting to Wi-Fi");
+  while (WiFi.status() != WL_CONNECTED)
+  {
+    Serial.print(".");
+    delay(300);
+  }
+  Serial.println();
+  Serial.print("Connected with IP: ");
+  Serial.println(WiFi.localIP());
+  Serial.println();
+
+  Serial.printf("Firebase Client v%s\n\n", FIREBASE_CLIENT_VERSION);
+
+  /* Assign the api key (required) */
+  config.api_key = API_KEY;
+
+  /* Assign the user sign in credentials */
+  auth.user.email = USER_EMAIL;
+  auth.user.password = USER_PASSWORD;
+
+  /* Assign the RTDB URL (required) */
+  config.database_url = DATABASE_URL;
+
+  /* Assign the callback function for the long running token generation task */
+  config.token_status_callback = tokenStatusCallback; //see addons/TokenHelper.h
+
+
+  Firebase.begin(&config, &auth);
+
+  //Comment or pass false value when WiFi reconnection will control by your code or third party library
+  Firebase.reconnectWiFi(true);
+
+  Firebase.setDoubleDigits(5);
 }
 
 void loop(){
-  internet();
   measure();
   lcd.clear();
   lcd.setCursor(0,0);
@@ -62,7 +115,11 @@ void loop(){
   lcd.print(bin_lvl);
   lcd.println("%");
 
-  Firebase.setInt("/level/GLevels",bin_lvl);
+    if (Firebase.ready() && (millis() - sendDataPrevMillis > 15000 || sendDataPrevMillis == 0)) {
+          sendDataPrevMillis = millis();
+
+         Serial.printf("Set int... %s\n", Firebase.setInt(fbdo, F("/test/int"), bin_lvl) ? "ok" : fbdo.errorReason().c_str());
+    }
 
   for(int i=0;i< Minute;i++){
     Serial.println("-------------------------");
@@ -89,18 +146,4 @@ void measure(){
   if (bin_lvl < 0) bin_lvl = 0;
 }
 
-void internet()
-{
-  if (WiFi.status() != WL_CONNECTED)
-  {
-    Serial.print("Attempting to connect to SSID: ");
-    Serial.println(ssid);
-    while (WiFi.status() != WL_CONNECTED)
-    {
-      WiFi.begin(ssid,pass);
-      Serial.print(".");
-      delay(5000);
-    }
-    Serial.println("\nConnected.");
-  }
-}
+
